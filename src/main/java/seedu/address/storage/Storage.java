728a247c0bfe87e77ac848bd1a9259a3f2b9df10
Adrian <quantumparadoxes@gmail.com>
2018-10-24 19:02:22 +0800

    Update Storage.java

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -70,7 +70,7 @@ public interface Storage extends AddressBookStorage, ExpenseBookStorage, UserPre
 
     /**
      * Saves the current version of the Expense Book to the hard disk.
-     *   Creates the addressData file if it is missing.
+     *   Creates the data file if it is missing.
      * Raises {@link DataSavingExceptionEvent} if there was an error during saving.
      */
     void handleExpenseBookChangedEvent(ExpenseBookChangedEvent abce);
